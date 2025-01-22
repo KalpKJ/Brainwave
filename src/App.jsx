aee3c5285423b0bac12595c9a23ce3301a040ab9
KalpKJ <kalpjoshi2003@gmail.com>
2025-01-21 21:53:40 -0600

    made a resuable button and anchor tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Radhe Krishna</h1>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className= "mt-10" >
+          Login
+        </Button>
+      </div>
+      <ButtonGradient />
     </>
   );
 }
